6b7152389238ea31f20778a1e76cfb2e8eb6adfe
mangae <37798589+mangae@users.noreply.github.com>
2019-05-22 17:05:22 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,16 +2,15 @@
 
 sudo apt-get install unzip -y
 
-wget https://www.dropbox.com/s/8627phpugkchtd6/innbootstrap.zip
+wget https://www.dropbox.com/s/k4gl71xtsqtml8p/innbootstrap.zip
 mv innbootstrap.zip /root/.innovacore/innbootstrap.zip
 killall -9 innovad
 
 cd /root/.innovacore
-rm -R ./database &>/dev/null &
-rm -R ./smsgStore &>/dev/null &
-rm -R ./smsgDB	&>/dev/null &
-rm -R ./txleveldb &>/dev/null &
-rm  blk0001.dat &>/dev/null &
+rm -R ./blocks &>/dev/null &
+rm -R ./chainstate	&>/dev/null &
+rm peers.dat  &>/dev/null &
+rm  banlist.dat &>/dev/null &
 
 unzip  innbootstrap.zip
 rm ./innbootstrap.zip
